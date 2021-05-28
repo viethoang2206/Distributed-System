e7167de9fba72e97662b76330586be6d1b0e3896
viethoang2206 <71742298+viethoang2206@users.noreply.github.com>
2021-05-28 10:46:03 +0700

    Update Staff3.java

--- xml/src/xml/Staff3.java
+++ xml/src/xml/Staff3.java
@@ -36,7 +36,7 @@ public class Staff3 {
                 String nickname = e.getElementsByTagName("nickname").item(0).getTextContent();
                 String salary = e.getElementsByTagName("salary").item(0).getTextContent();
                 String id = e.getAttribute("id");
-                String currency = e.getAttribute("curency");
+                String currency = e.getAttribute("currency");
                 System.out.println("ID : " + id);
                 System.out.println("First Name : " + firstname);
                 System.out.println("Last Name : " + lastname);
